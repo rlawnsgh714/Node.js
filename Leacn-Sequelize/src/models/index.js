e9179faf9bfc82c7e9f5a6a92f617ebd11cfce62
kimjunho <junho07140714@dgsw.hs.kr>
2023-03-13 15:25:56 +0900

    Add : Learn Sequeilze

---
+++ Leacn-Sequelize/src/models/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.passowrd,
+  config
+);
+
+db.sequelize = sequelize;
+
+module.exports = db;
